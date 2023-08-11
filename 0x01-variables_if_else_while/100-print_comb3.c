9148707b996a95f1825f5a026b89371527ca9849
God'spower Agboola <godpoweragboola@gmail.com>
2023-08-11 14:44:23 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Prints all possible diffrent combination of two digit
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int k;
+	int l;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+		for (l = '0'; l <= '9'; l++)
+		{
+			if (k < l)
+			{
+			putchar(k);
+			putchar(l);
+
+			if (k != '8' || (k == '8' && l != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
