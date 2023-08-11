778e490cfa67131b11d59a5cf773ac4d22b514a5
God'spower Agboola <godpoweragboola@gmail.com>
2023-08-11 14:18:35 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combination of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int g;
+
+	for (g = '0'; g < '9'; g++)
+	{
+		putchar(g);
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
