aa45fb5869f23f83e4e5d4db32b68f760540922d
God'spower Agboola <godpoweragboola@gmail.com>
2023-08-11 08:48:06 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Sinle digit number of base ten
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
