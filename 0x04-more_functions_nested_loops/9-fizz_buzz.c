c009a5232115907ac57c102572f1281bc32c50ff
God'spower Agboola <godpoweragboola@gmail.com>
2023-08-17 09:45:25 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - entry point
  *
  * Description: prints the numbers 1 - 100 w/
  * Fizz for multiples of 3, Buzz for multiples of 5,
  * and FizzBuzz for multiples of both
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
