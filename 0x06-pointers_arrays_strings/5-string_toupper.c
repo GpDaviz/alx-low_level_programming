723dc00157cdedc09ffd91d9ec2355aaef084519
God'spower Agboola <godpoweragboola@gmail.com>
2023-08-24 06:29:41 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,13 +12,13 @@
 char *string_toupper(char *c)
 {
 	int i;
-
 	for (i = 0; c[i] != '\0'; i++)
+
 	{
-		if (c[i] > 96 && c[i] < 123)
+		if (c[i] >= 'a' && c[i] <= 'z')
 		{
-			c[i] -= 32;
+			c[i] -= ('a' - 'A');
 		}
 	}
-	return (0);
+	return (c);
 }
