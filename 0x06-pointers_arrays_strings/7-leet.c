0e6c69a133fa64fbc29df46bfa4f52a840530dcb
God'spower Agboola <godpoweragboola@gmail.com>
2023-08-24 17:53:09 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,7 +3,7 @@
 /**
  * leet - Encode a string into 1337.
  * @s: The string to be encoded.
- * Return: A pointer to the encoded string.
+ * Return: 's' A pointer to the encoded string.
  */
 char *leet(char *s)
 {
