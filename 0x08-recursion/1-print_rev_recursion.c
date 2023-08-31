65f325355757ef92cf03d40d709a4610c9840f2a
God'spower Agboola <godpoweragboola@gmail.com>
2023-08-30 19:49:13 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Write a function that prints a string in reverse
- *
- * @s: first value
- *
+ * _print_rev_recursion - function that prints a string in reverse
+ *@s: first value
  *
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
