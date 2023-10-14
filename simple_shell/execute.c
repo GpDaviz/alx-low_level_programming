62abf7177436da9a586eae4cd21c92ba068ea397
God'spower Agboola <godpoweragboola@gmail.com>
2023-10-14 07:25:42 -0700

    execute.c

---
+++ simple_shell/execute.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/***
+ * execute_mycommand - crate your own command
+ *
+ * Return: NULL
+ */
+
+void execute_mycommand(const char *mycommand);
+{
+	pid_t child_pid =fork( );
+
+	if (child_pid == -1)
+	{
+		perror("fork");
+		exit (EXIT_FAILURE);
+	}
+	else if (child_pid ==0)
+	{
+		execev (mycommand, mycommand, (char *)NULL);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait (NULL);
+	}
+}
